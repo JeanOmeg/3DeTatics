c36e58660ccd9c8056e5d7377a70718911531cfd
jeanfernandespro <jean.fernandes@prodigitalhr.com.br>
2025-10-12 10:14:22 -0300

    first deploy

--- quasar.config.ts
+++ quasar.config.ts
@@ -50,7 +50,7 @@ export default defineConfig((/* ctx */) => {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: '/',
+      publicPath: '/3DeTatics/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
